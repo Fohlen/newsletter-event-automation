f74dc2d433322b500d2880b90ea27a9853217514
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2023-07-23 21:47:23 +0200

    Add visible_text test

--- tests/test_read_mbox.py
+++ tests/test_read_mbox.py
@@ -1,4 +1,9 @@
+import html
+
+import pytest
+
 from src.message import read_mbox
+from src.visible_text_parser import VisibleTextParser
 
 
 def test_read_mbox(data_dir):
@@ -9,3 +14,14 @@ def test_read_mbox(data_dir):
         "___________________________"
     ))
     assert len(messages) == 20
+
+
+def test_visible_text(data_dir):
+    with (data_dir / "visible_text.html").open() as fp:
+        raw_content = fp.read()
+        html_parser = VisibleTextParser()
+        html_parser.feed(raw_content)
+        content = html_parser.get_visible_text()
+        escaped_content = html.unescape(content).strip()
+        # this hardcoded number can be retrieved with document.body.innerText.length
+        assert len(escaped_content) == pytest.approx(1433, rel=0.1)
